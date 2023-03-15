b3a59f0c8ebfc0059e85f7236726bd5d8c1cbdbd
DABADIE <romain.dabadie@cgi.com>
2023-03-15 13:57:20 +0100

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092", // Replace with the appropriate bootstrap server(s)
+            GroupId = "my-group", // Replace with the name of your consumer group
+            AutoOffsetReset = AutoOffsetReset.Earliest // Replace with the desired offset reset policy
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe("demo-topic"); // Replace with the name of the Kafka topic you want to consume messages from
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // Prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    var result = consumer.Consume(cts.Token);
+
+                    Console.WriteLine($"Consumed message '{result.Message.Value}' at: '{result.TopicPartitionOffset}'.");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // The consumer was stopped.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System.Net;
+
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        var message = new Message<Null, string>
+        {
+            Value = "Hello, Kafka!" // Replace with the message you want to send
+        };
+
+        var result = await producer.ProduceAsync("demo-topic", message);
+
+        Console.WriteLine($"Delivered '{result.Value}' to '{result.TopicPartitionOffset}'");
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+    }
+}
